2ee5b5dac8d1e929bd2c213d7586f38e4585f919
Dennie Declercq <dennie@ddsoft.be>
2022-11-28 21:51:43 +0100

    Add project files.

---
+++ Model/Enums/TripType.cs
@@ -0,0 +1,14 @@
+﻿namespace Model.Enums
+{
+    public enum TripType
+    {
+        Kerstmarkt,
+        Kermis,
+        Optreden,
+        Sauna,
+        Pretpark,
+        Zwemparadijs,
+        Expo,
+        Museum
+    }
+}
---
+++ Model/Trip.cs
@@ -0,0 +1,59 @@
+﻿using Model.Enums;
+using System.Collections;
+
+namespace Model
+{
+   
+
+    public class Trips
+    {
+        private List<Trip> _trips;
+        public Trips()
+        {
+            _trips = new List<Trip>();
+        }
+        public void AddTrip(Trip trip)
+        {
+            if (trip != null)
+                _trips.Add(trip);
+            else
+                throw new Exception();
+        }
+        
+        public void AddTrips(List<Trip> trips)
+        {
+            if (trips != null)
+                _trips.AddRange(trips);
+            else
+                throw new Exception();
+        }
+        
+        public IEnumerable<Trip> GetTrips()
+        {
+            return _trips;
+        }
+        
+    }
+    public class Trip
+    {
+        public Trip()
+        { }
+        
+        public Trip(DateOnly date, string tripName, string city, string country, TripType type)
+        {
+            Date = date;
+            TripName = tripName;
+            City = city;
+            Country = country;
+            Type = type;
+        }
+        public DateOnly Date { get; set; }
+        public string TripName { get; set; } = "";
+
+        public string City { get; set; } = "";
+        public string Country { get; set; } = "";
+
+        public TripType Type { get; set; }
+    }
+
+}
