ced220d15767ffbff09d7b6b831a1c9d33e867b8
Dennie Declercq <dennie@ddsoft.be>
2023-04-04 21:09:03 +0200

    tiny refactorings

--- Model/Photo.cs
+++ Model/Photo.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model
+﻿namespace Model
 {
     public record Photo
     {
         public string FileName { get; set; } = "";
-        public string URI { get; set; }
+        public string URI { get; set; } = "";
         public string? ALT { get; set; }
 
     }
